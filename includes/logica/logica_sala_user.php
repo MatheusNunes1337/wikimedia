6731bc7e7cecff23dd78c9096c6f629f5842209e
Matheus Nunes <matheusyagami147@gmail.com>
2019-10-10 16:01:02 -0300

    crud de postagem do arquivo sala_user finalizado

--- includes/logica/logica_sala_user.php
+++ includes/logica/logica_sala_user.php
@@ -7,13 +7,22 @@
     $obj = json_decode($json);
 
     if($_SERVER['REQUEST_METHOD'] == 'GET') {
-
+    	if($obj->funcao == 'buscar post') {
+    		$array = array($obj->post_id);
+    		$posts = pesquisarPostagem($conexao, $array);
+    		if($posts) {
+    			echo json_encode($posts);
+    		} else {
+    			$status = array('status'=>'falha', 'mensagem'=>'Hmm, parece que não há nenhuma postagem com esse titulo. Tente novamente');
+    			echo json_encode($status);
+    		}
+    	}
 
 
     	die();
     }
     
-    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if($_SERVER['REQUEST_METHOD'] == 'POST') { //possibilidade de apostas uma midia por post
     	if($obj->funcao == 'criar post') {
     		$array = array($obj->titulo, $obj->conteudo);
     		$postagem = criarPostagem($conexao, $array);
@@ -32,27 +41,68 @@
 	            $status = array('status'=>'falha', "mensagem"=>"Houve um erro ao tentar criar a publicação. Tente novamente");
 	        }
 	         echo json_encode($status);
-	    	}
+	    	
 	   	} else if($obj->funcao == 'comentar') {
-
+	   		$array = array($obj->conteudo, $obj->user_id, $obj->post_id);
+	   		$okay = criarComentario($conexao, $array);
+	   		if($okay) {
+	   			$status = array('status'=>'sucesso');		
+	   		} else {
+	   			$status = array('status'=>'falha', "mensagem"=>"Houve um erro ao tentar comentar nesta publicação. Tente novamente");
+	   		}
+	   		echo json_encode($status);
 	   	} 
     	die();
     }
 
     if($_SERVER['REQUEST_METHOD'] == 'PUT') {
-
+    	if($obj->funcao == 'editar post') {
+    		$array = array($obj->post_id, $obj->titulo, $obj->conteudo);
+    		$okay = editarPostagem($conexao, $array);
+    		if($okay) {
+    			if($obj->nomeMidia) {
+    				$array = array($obj->nomeMidia, $obj->post_id);
+    				$editada = editarMidia($conexao, $array); //caso o post não tivesse midia antes disso. Agora passará a ter.
+    				if($editada) {
+    					$status = array('status'=>'sucesso');
+    				} else {
+    					$status = array('status'=>'erro ao tentar editar uma midia existente');
+    				}
+    			} else {
+    				$array = array($obj->post_id);
+    				$existe = verificaPostagem($conexao, $array); //verifica se a postagem possui alguma midia vinculada a ela.
+    				if($existe) {
+    					$deletada = deletarMidia($conexao, $array); //deleta a midia da postagem
+    					if($deletada) {
+    						$status = array('status'=>'sucesso');
+	    				} else {
+	    					$status = array('status'=>'falha', 'mensagem'=>'Falha ao tentar excluir a midia da postagem');
+	    				}
+    				}
+    			}
+    		} else {
+    			$status = array('status'=>'falha', "mensagem"=>"Houve um erro ao tentar editar esta publicação. Tente novamente");	
+    		}
+    		echo json_encode($status);
+    	}
 
 
     	die();
     }
 
     if($_SERVER['REQUEST_METHOD'] == 'delete') {
-
-
-
-
-    }	
-	
+    	if(isset($_REQUEST['deletar_post'])) {
+    		$post_id = $_REQUEST['post_id'];
+    		$array = array($post_id);
+    		$deletado = deletarPostagem($conexao, $array);
+    		if($deletado) {
+    			$status = array('status'=>'sucesso', 'mensagem'=>'Essa postagem foi deletada com sucesso');
+    		} else {
+    			$status = array('status'=>'falha', "mensagem"=>"Hmm, parece que não foi possivel deletar essa postagem. Tente novamente");	
+    		}
+    		echo json_encode($status);
+    	}
+    }
 	
 	
 
