8327727d15168a097921db0eff847e9f92b570d5
Matheus Nunes <matheusyagami147@gmail.com>
2019-10-19 14:04:44 -0300

    diretório de imagens criado

--- forms/user/cadastro.php
+++ forms/user/cadastro.php
@@ -1,25 +1,27 @@
 <link rel="stylesheet" type="text/css" href="../../includes/componentes/estilo.css">
-<form method="POST" action="../../includes/logica/logica_usuario.php" id="cadastro">
-	<div class="cabeçalho">
-		<p>Sign up</p>
-	</div>
-	<div id="campos">
-		<div class="campo">
-			<input type="text" name="username" id="username" placeholder="username">
-			<small>ssssss</small>
+<body style="background-image: url(../../includes/componentes/imagens/interface/login_background.jpg);">
+	<form method="POST" action="../../includes/logica/logica_usuario.php" id="cadastro">
+		<div class="cabeçalho">
+			<p>Cadastro</p>
 		</div>
-		<div class="campo">
-			<input type="email" name="email" id="email" placeholder="email">
-			<small>dddddd</small>
-		</div>
-		<div class="campo">
-			<input type="password" name="senha" id="senha" placeholder="senha">
-			<small>ssssss</small>
-		</div>
-		<div class="campo" style="margin-top: 25px;">
-			<button name="cadastrar">Cadastrar</button>
-		</div>
-	<p id="possuir_conta">
-		<a href="login.php">Já possui uma conta?</a>
-	</p>
-</form>
+		<div id="campos">
+			<div class="campo">
+				<input type="text" name="username" id="username" placeholder="username">
+				<small>ssssss</small>
+			</div>
+			<div class="campo">
+				<input type="email" name="email" id="email" placeholder="email">
+				<small>dddddd</small>
+			</div>
+			<div class="campo">
+				<input type="password" name="senha" id="senha" placeholder="senha">
+				<small>ssssss</small>
+			</div>
+			<div class="campo" style="margin-top: 25px;">
+				<button name="cadastrar">Cadastrar</button>
+			</div>
+		<p id="possuir_conta">
+			<a href="login.php">Já possui uma conta?</a>
+		</p>
+	</form>
+</body>
--- forms/user/login.php
+++ forms/user/login.php
@@ -1,19 +1,21 @@
 <link rel="stylesheet" type="text/css" href="../../includes/componentes/estilo.css">
-<form method="POST" action="../../includes/logica/logica_usuario.php" id="login">
-	<div class="cabeçalho">
-		<p>Login</p>
-	</div>
-	<div id="campos">
-		<div class="campo">
-			<input type="text" name="username" id="username" placeholder="username">
+<body style="background-image: url(../../includes/componentes/imagens/interface/login_background.jpg);">
+	<form method="POST" action="../../includes/logica/logica_usuario.php" id="login">
+		<div class="cabeçalho">
+			<p>Login</p>
 		</div>
-		<div class="campo">
-			<input type="password" name="senha" id="senha" placeholder="senha">
-		</div>
-		<div class="campo" style="margin-top: 25px;">
-			<button id="logar" name="logar">Entrar</button>
-		</div>
-	<p id="possuir_conta">
-		<a href="cadastro.php">Não possui uma conta ainda?</a>
-	</p>
-</form>
+		<div id="campos">
+			<div class="campo">
+				<input type="text" name="username" id="username" placeholder="username">
+			</div>
+			<div class="campo">
+				<input type="password" name="senha" id="senha" placeholder="senha">
+			</div>
+			<div class="campo" style="margin-top: 25px;">
+				<button id="logar" name="logar">Entrar</button>
+			</div>
+		<p id="possuir_conta">
+			<a href="cadastro.php">Não possui uma conta ainda?</a>
+		</p>
+	</form>
+</body>
