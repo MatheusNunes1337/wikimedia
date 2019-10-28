cd99539fa62655eacb1766b2c401a81731efea6f
Matheus Nunes <matheusyagami147@gmail.com>
2019-10-28 12:53:36 -0300

    diretório de arquivos html criado

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
 <h1>Olá</h1>
+<div id="root"></div>
+<button onclick="carregar()">clique</button>
+<script type="text/javascript">
+	let r = document.getElementById('root');
+	function carregar() {
+		fetch('formulario.html'
+		)
+		.then(response => response.text())
+		.then(html => {
+			r.innerHTML = html; 		
+	 	})
+	  	.catch(error => {
+	  		console.log(error);
+	    }); 
+	
+	}
+</script>
